fcd21d667f181988053d5d48cc5eab29ffbacce5
gcuauhtemoc <gerardocuauhtemoc18@gmail.com>
2024-12-15 00:03:32 -0600

    Add files via upload

---
+++ test/rest/api_test.py
@@ -0,0 +1,67 @@
+import http.client
+import os
+import unittest
+from urllib.request import urlopen
+
+import pytest
+
+BASE_URL = "http://localhost:5000"
+BASE_URL_MOCK = "http://localhost:9090"
+DEFAULT_TIMEOUT = 2  # in secs
+
+@pytest.mark.api
+class TestApi(unittest.TestCase):
+    def setUp(self):
+        self.assertIsNotNone(BASE_URL, "URL no configurada")
+        self.assertTrue(len(BASE_URL) > 8, "URL no configurada")
+
+    def test_api_add(self):
+        url = f"{BASE_URL}/calc/add/1/2"
+        response = urlopen(url, timeout=DEFAULT_TIMEOUT)
+        self.assertEqual(
+            response.status, http.client.OK, f"Error en la petición API a {url}"
+        )
+        self.assertEqual(
+            response.read().decode(), "3", "ERROR ADD"
+        )
+
+    def test_api_sqrt(self):
+        url = f"{BASE_URL_MOCK}/calc/sqrt/64"
+        response = urlopen(url, timeout=DEFAULT_TIMEOUT)
+        self.assertEqual(
+            response.status, http.client.OK, f"Error en la petición API a {url}"
+        )
+        self.assertEqual(
+            response.read().decode(), "8", "ERROR SQRT"
+        )
+
+    def test_api_multiply(self):
+        url = f"{BASE_URL}/calc/multiply/5/2"
+        response = urlopen(url, timeout=DEFAULT_TIMEOUT)
+        self.assertEqual(
+            response.status, http.client.OK, f"Error en la petición API a {url}"
+        )
+        self.assertEqual(
+            response.read().decode(), "10", "ERROR MULTIPLY"
+        )
+
+    def test_api_divide(self):
+        url = f"{BASE_URL}/calc/divide/8/2"
+        response = urlopen(url, timeout=DEFAULT_TIMEOUT)
+        self.assertEqual(
+            response.status, http.client.OK, f"Error en la petición API a {url}"
+        )
+        self.assertEqual(
+            response.read().decode(), "4.0", "ERROR DIVIDE"
+        )
+   def test_api_divide_cero(self):
+       url = f"{BASE_URL}/calc/divide/8/0"
+       with self.assertRaises(Exception) as context:
+           response = urlopen(url, timeout=DEFAULT_TIMEOUT)
+       self.assertEqual(
+           context.exception.code, http.client.NOT_ACCEPTABLE, "ERROR-DIVISION ENTRE 0"
+       )
+
+
+if __name__ == "__main__":  # pragma: no cover
+    unittest.main()
